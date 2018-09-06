af2618d651451d707d9bb69b20aa72c949f5232d
kongaloosh <hi@kongaloosh.com>
2018-09-06 07:11:46 -0600

    added auto-install of requirements

--- setup.py
+++ setup.py
@@ -6,4 +6,5 @@ setup(
     packages=['python-webmention'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
+    install_requires=open('requirements.txt').read().split('\n'),
 )
